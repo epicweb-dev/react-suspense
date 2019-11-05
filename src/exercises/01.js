9da7d3d482e52ecf87b59ff90c0a78561e6f3b46
Kent C. Dodds <kent@doddsfamily.us>
2019-11-05 16:06:49 -0700

    01 exercise ready

--- src/exercises/01.js
+++ src/exercises/01.js
@@ -3,15 +3,83 @@
 // http://localhost:3000/isolated/exercises/01
 
 import React from 'react'
+// 🐨 you'll need to import the fetchPokemon function
+// 💰 here you go:
+// import fetchPokemon from '../fetch-pokemon'
+// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
+import {ErrorBoundary} from '../utils'
+
+// By default, all fetches are mocked so we can control the time easily.
+// You can adjust the fetch time with this:
+// window.FETCH_TIME = 3000
+// If you want to make an actual network call for the pokemon
+// then uncomment the following line
+// window.fetch.restoreOriginalFetch()
+// Note that by doing this, the FETCH_TIME will no longer be considered
+// and if you want to slow things down you should use the Network tab
+// in your developer tools to throttle your network to something like "Slow 3G"
+
+// 🐨 create the following mutable variable references (using let):
+// pokemon, pokemonError, pokemonPromise
+
+// We don't need the app to be mounted to know that we want to fetch the pokemon
+// named "pikachu" so we can go ahead and do that right here.
+// 🐨 assign the pokemonPromise variable to a call to fetchPokemon('pikachu')
+
+// 🐨 when the promise resolves, set the pokemon variable to the resolved value
+// 🐨 if the promise fails, set the pokemonError variable to the error
+
+function PokemonInfo() {
+  // 🐨 if pokemonError is defined, then throw it here
+  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
+  // 💰 (no, for real. Like: `throw pokemonPromise`)
+
+  // if the code gets it this far, then the pokemon variable is defined and
+  // rendering can continue!
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <section>
+        <h2>
+          {pokemon.name}
+          <sup>{pokemon.number}</sup>
+        </h2>
+      </section>
+      <section>
+        <ul>
+          {pokemon.attacks.special.map(attack => (
+            <li key={attack.name}>
+              <label>{attack.name}</label>:{' '}
+              <span>
+                {attack.damage} <small>({attack.type})</small>
+              </span>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <small className="pokemon-info__fetch-time">{pokemon.fetchedAt}</small>
+    </div>
+  )
+}
 
 function App() {
-  return <div>TODO</div>
+  return (
+    <div className="pokemon-info">
+      <ErrorBoundary>
+        <React.Suspense fallback={<div>Loading Pokemon...</div>}>
+          <PokemonInfo />
+        </React.Suspense>
+      </ErrorBoundary>
+    </div>
+  )
 }
 
 /*
 🦉 Elaboration & Feedback
 After the instruction, copy the URL below into your browser and fill out the form:
-http://ws.kcd.im/?ws=Concurrent%20React&e=TODO&em=
+http://ws.kcd.im/?ws=Concurrent%20React&e=Simple%20Data-fetching&em=
 */
 
 ////////////////////////////////////////////////////////////////////
@@ -21,8 +89,9 @@ http://ws.kcd.im/?ws=Concurrent%20React&e=TODO&em=
 //                                                                //
 ////////////////////////////////////////////////////////////////////
 
-function Usage() {
-  return <App />
-}
+export default App
 
-export default Usage
+/*
+eslint
+  no-undef: off
+*/
