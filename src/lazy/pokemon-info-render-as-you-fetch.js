8b81bc24ff76343f9c7f4003db3b601edbcd4c96
lauchness <51837850+lauchness@users.noreply.github.com>
2020-01-21 19:16:34 -0400

    Updating md file and adding component for 2nd extra-credit on 05 (#2)
    * Updating md file and adding component for 2nd extra-credit on 05
    
    * Update 05-extra.2.js
    
    Co-authored-by: Kent C. Dodds <me+github@kentcdodds.com>

---
+++ src/lazy/pokemon-info-render-as-you-fetch.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {PokemonDataView} from '../utils'
+
+function PokemonInfo({pokemonResource}) {
+  const pokemon = pokemonResource.data.read()
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemonResource.image.read()} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  )
+}
+
+export default PokemonInfo
