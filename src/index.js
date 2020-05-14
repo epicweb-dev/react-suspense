8524870c92cf0130d0a62de2a67dbf6d83a7e684
Kent C. Dodds <me+github@kentcdodds.com>
2020-05-14 17:07:54 -0600

    update all the things

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import React from 'react'
 import './hacks/fetch'
 import './styles.css'
 import codegen from 'codegen.macro'
+
+// 🚨 PLEASE HEED ALL WARNINGS AROUND THIS WORKSHOP TELLING YOU THAT THIS
+// IS UNSTABLE SOFTWARE...
+// I just don't want to have to type "unstable_" prefixes everywhere
+React.useTransition = React.unstable_useTransition
+React.SuspenseList = React.unstable_SuspenseList
 
 codegen`module.exports = require('@kentcdodds/react-workshop-app/codegen')({
   options: {concurrentMode: true}
