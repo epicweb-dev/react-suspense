2bc8315c04ca7876d294df8ba7a91cedf743e6d7
Kent C. Dodds <me+github@kentcdodds.com>
2024-04-24 14:01:22 -0600

    update to epicshop

---
+++ epicshop/update-deps.sh
@@ -0,0 +1,8 @@
+npx npm-check-updates --dep prod,dev --upgrade --root
+cd epicshop && npx npm-check-updates --dep prod,dev --upgrade --root
+cd ..
+rm -rf node_modules package-lock.json ./epicshop/package-lock.json ./epicshop/node_modules ./exercises/**/node_modules
+npm install
+npm run setup
+npm run typecheck
+npm run lint --fix
--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
-/** @type {import('@types/eslint').Linter.BaseConfig} */
-module.exports = {
-	extends: [
-		'@remix-run/eslint-config',
-		'@remix-run/eslint-config/node',
-		'prettier',
-	],
+/** @type {import('@types/eslint').Linter.Config} */
+export default {
+	languageOptions: { parser: await import('@typescript-eslint/parser') },
+	plugins: {
+		'@typescript-eslint': (await import('@typescript-eslint/eslint-plugin'))
+			.default,
+		import: (await import('eslint-plugin-import')).default,
+		'react-hooks': (await import('eslint-plugin-react-hooks')).default,
+	},
 	rules: {
-		'@typescript-eslint/no-unused-vars': 'off',
-		'jsx-a11y/alt-text': 'off',
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
 		// playwright requires destructuring in fixtures even if you don't use anything 🤷‍♂️
 		'no-empty-pattern': 'off',
-		'@typescript-eslint/no-unused-expressions': 'off',
-		'no-unused-expressions': 'off',
 		'@typescript-eslint/consistent-type-imports': [
 			'warn',
 			{
